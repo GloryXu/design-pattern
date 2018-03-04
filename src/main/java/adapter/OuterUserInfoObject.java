42847e57b89400486a23f565ec96c7f17aa3687a
xugr <xugr>
2018-03-04 17:30:48 +0800

    模板方法模式

--- src/main/java/adapter/OuterUserInfoObject.java
+++ src/main/java/adapter/OuterUserInfoObject.java
@@ -1,22 +1,21 @@
 package adapter;
 
-import java.util.Map;
-
 /**
- * 把OuterUser包装成UserInfo
- *
- * 类适配器
+ * 对象适配器
  */
-public class OuterUserInfo extends OuterUser implements IUserInfo {
-    private Map<String, String> baseInfo = super.getUserBaseInfo(); // 员工的基本信息
-    private Map<String, String> homeInfo = super.getUserHomeInfo(); // 员工的家庭信息
-    private Map<String, String> officeInfo = super.getUserOfficeInfo(); // 工作信息
+public class OuterUserInfoObject implements IUserInfo {
 
-   /*
-    * 家庭地址
-    */
+    private IOuterUser outerUser;
+
+    public OuterUserInfoObject(IOuterUser outerUser) {
+        this.outerUser = outerUser;
+    }
+
+    /*
+        * 家庭地址
+        */
     public String getHomeAddress() {
-        String homeAddress = this.homeInfo.get("homeAddress");
+        String homeAddress = outerUser.getUserHomeInfo().get("homeAddress");
         System.out.println(homeAddress);
         return homeAddress;
     }
@@ -25,7 +24,7 @@ public class OuterUserInfo extends OuterUser implements IUserInfo {
     * 家庭电话号码
     */
     public String getHomeTelNumber() {
-        String homeTelNumber = this.homeInfo.get("homeTelNumber");
+        String homeTelNumber = outerUser.getUserHomeInfo().get("homeTelNumber");
         System.out.println(homeTelNumber);
         return homeTelNumber;
     }
@@ -34,7 +33,7 @@ public class OuterUserInfo extends OuterUser implements IUserInfo {
     *职位信息
     */
     public String getJobPosition() {
-        String jobPosition = this.officeInfo.get("jobPosition");
+        String jobPosition = outerUser.getUserOfficeInfo().get("jobPosition");
         System.out.println(jobPosition);
         return jobPosition;
     }
@@ -43,7 +42,7 @@ public class OuterUserInfo extends OuterUser implements IUserInfo {
     * 手机号码
     */
     public String getMobileNumber() {
-        String mobileNumber = this.baseInfo.get("mobileNumber");
+        String mobileNumber = outerUser.getUserBaseInfo().get("mobileNumber");
         System.out.println(mobileNumber);
         return mobileNumber;
     }
@@ -52,7 +51,7 @@ public class OuterUserInfo extends OuterUser implements IUserInfo {
     * 办公电话
     */
     public String getOfficeTelNumber() {
-        String officeTelNumber = this.officeInfo.get("officeTelNumber");
+        String officeTelNumber = outerUser.getUserOfficeInfo().get("officeTelNumber");
         System.out.println(officeTelNumber);
         return officeTelNumber;
     }
@@ -61,7 +60,7 @@ public class OuterUserInfo extends OuterUser implements IUserInfo {
     * 员工的名称
     */
     public String getUserName() {
-        String userName = this.baseInfo.get("userName");
+        String userName = outerUser.getUserBaseInfo().get("userName");
         System.out.println(userName);
         return userName;
     }
